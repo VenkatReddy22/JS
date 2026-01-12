371b5563763efefc08e4ea6fc078f3e4ddcc87f3
Venkat Reddy <159973985+VenkatReddy22@users.noreply.github.com>
2026-01-12 15:01:19 -0500

    Refactor script.js for improved readability
    Refactor variable names, fix typos, and improve formatting.

--- To do/script.js
+++ To do/script.js
@@ -1,31 +1,31 @@
 // select dom elements
 const input = document.getElementById('todo-input')
-const addbtn = document.getElementById('add-btn')
+const addBtn = document.getElementById('add-btn')
 const list = document.getElementById('todo-list')
 
 // try to load todo from saved storage
 const saved = localStorage.getItem('todos');
-const todos = saved? JSON.parse(saved): [];
+const todos = saved ? JSON.parse(saved) : [];
 
-function saveTodos(){
+function saveTodos() {
     //save current todos array to local storage 
-    localStorage.setItem('todos',JSON.stringify(todos));
+    localStorage.setItem('todos', JSON.stringify(todos));
 
 }
 
 //creating a dom node for todo object and appending that node to the list.
-function createTodoNode(todo, index){
+function createTodoNode(todo, index) {
     const li = document.createElement('li');
 
     //checkbox to toggle completion of an item in list
     const checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
     checkbox.checked = !!todo.completed;
-    checkbox.addEventListener("change", ()=>{
+    checkbox.addEventListener("change", () => {
         todo.completed = checkbox.checked;
 
         // adding a visual feedback: strike-through text decoration
-        textspan.style.textDecoration = todo.completed? 'line-through' : "";
+        textSpan.style.textDecoration = todo.completed ? 'line-through' : "";
         saveTodos();
     })
 
@@ -33,38 +33,72 @@ function createTodoNode(todo, index){
     const textSpan = document.createElement("span");
     textSpan.textContent = todo.text;
     textSpan.style.margin = ' 2px 10px';
-    if(todo.completed){
+    if (todo.completed) {
         textSpan.style.textDecoration = 'line-through';
     }
 
     //ADD: when double clicked on text span, prompt user to edit the text
-    textSpan.addEventListener('dblclick', ()=>{
-        const newText = prompt('Edit todo', todo.next);
-        if(newText !== null){
+    textSpan.addEventListener('dblclick', () => {
+        const newText = prompt('Edit todo', todo.text);
+        if (newText !== null) {
             todo.text = newText.trim();
-            textSpan.textConent = todo.text;
+            textSpan.textContent = todo.text;
             saveTodos();
+            // render();
         }
     })
-    
 
+    //adding a delete button to list
 
+    const delBtn = document.createElement('button');
+    delBtn.className = "delete-btn";
+    delBtn.textContent = "Delete";
+    delBtn.addEventListener('click', () => {
+        todos.splice(index, 1);
+        render();
+        saveTodos();
+
+    })
+
+    li.appendChild(checkbox);
+    li.appendChild(textSpan);
+    li.appendChild(delBtn);
+    return li;
 
 }
-//adding a delete button to list
 
-const delBtn = document.createElement('button')
-delBtn.textContent = "Delete";
-delBtn.addEventListener('click',()=>{
-    todos.splice(index,1);
-    render();
-    saveTodos();
-})
 
 //render the whole todo list from todos array
-function render(){
-    list.innerHtml = '';
+function render() {
+    list.innerHTML = '';
 
     //Recreating each item
-    
+    todos.forEach((todo, index) => {
+        const node = createTodoNode(todo, index);
+        list.appendChild(node);
+    });
+
 }
+
+// ADD: add button to add a task into todo list
+function addTodo(){
+    const text = input.value.trim();
+    if(!text){
+        // alert("Please enter a valid todo item.");
+        return;
+    }
+    
+    //push a new todo object
+    todos.push({text: text, completed: false});
+    input.value = '';
+    render()
+    saveTodos()
+}
+
+addBtn.addEventListener("click", addTodo)
+input.addEventListener('keydown', (e)=>{
+    if(e.key == 'Enter'){
+        addTodo();
+    }
+})
+render();
